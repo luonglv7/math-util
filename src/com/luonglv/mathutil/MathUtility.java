e8738e1a825bf1a993cceeb7ed93d8d28d5d8c17
luonglv7 <luonglv7@gmail.com>
2021-03-25 15:44:30 +0700

    Update core code & Green 4EVER

--- src/com/luonglv/mathutil/MathUtility.java
+++ src/com/luonglv/mathutil/MathUtility.java
@@ -25,7 +25,7 @@ public class MathUtility {
         //song sot den day n se la tu 0 - 20
         if(n == 0 || n==1) return 1; //0!, 1! = 1;
         
-        long result = 0;
+        long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
